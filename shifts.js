287023bbcea35159af165b07bcad917df1c0592a
Jojaloy <johnkurttrollz089@gmail.com>
2026-09-21 17:36:12 +0800

    Add overtime pay for shifts over 8 hours - realista.johnkurt

--- shifts.js
+++ shifts.js
@@ -2,8 +2,15 @@ function isValidShift(hours) {
   return hours > 0 && hours <= 24;
 }
 
-function calculatePay(hours, rate) {
-  return Math.floor(hours * rate);
+function calculatePay(hours, hourlyRate) {
+  const regularHoursLimit = 8;
+  if (hours <= regularHoursLimit) {
+    return hours * hourlyRate;
+  }
+  const regularPay = regularHoursLimit * hourlyRate;
+  const overtimeHours = hours - regularHoursLimit;
+  const overtimePay = overtimeHours * (hourlyRate * 1.5);
+  return regularPay + overtimePay;
 }
 
 module.exports = { isValidShift, calculatePay };
